8fdff3c31a491de7fc8e4b0619f1aa00cba7b99f
Timeless <leongandy027@gmail.com>
2023-12-29 19:16:07 +0800

    test

--- sst.config.ts
+++ sst.config.ts
@@ -15,7 +15,7 @@ export default {
           DATABASE_URL : process.env.DATABASE_URL!,
           NEXT_PUBLIC_SOCKET_URL : process.env.NEXT_PUBLIC_SOCKET_URL!,
           NEXTAUTH_URL : process.env.NEXTAUTH_URL!,
-          NEXTAUTH_SECRET : process.env.NEXTAUTH_SECFRET!,
+          NEXTAUTH_SECRET : process.env.NEXTAUTH_SECRET!,
           NEXTAUTH_GITHUB_ID : process.env.NEXTAUTH_GITHUB_ID!,
           NEXTAUTH_GITHUB_SECRET : process.env.NEXTAUTH_GITHUB_SECRET!,
         }
