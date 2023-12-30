c3116df0059c4c2f3c64e4846b7764c4b3a0c4e7
Timeless <leongandy027@gmail.com>
2023-12-30 15:51:47 +0800

    minor inconveniences v2

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,6 @@
 import { type SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
-// import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 
 export default {
   config(_input) {
@@ -13,13 +13,13 @@ export default {
     app.setDefaultRemovalPolicy("destroy");
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site", {
-        // customDomain: {
-        //   domainName: "whiteboard.timeles.codes",
-        //   isExternalDomain: true,
-        //   cdk: {
-        //     certificate: Certificate.fromCertificateArn(stack, "3dd2a628-1454-4ebf-8d66-9d59fa4174e4", "arn:aws:acm:us-east-1:684706882628:certificate/3dd2a628-1454-4ebf-8d66-9d59fa4174e4"),
-        //   },
-        // },
+        customDomain: {
+          domainName: "whiteboard.timeles.codes",
+          isExternalDomain: true,
+          cdk: {
+            certificate: Certificate.fromCertificateArn(stack, "fb228fdb-3d42-4c8f-a1d8-4b47887ecfd1", "arn:aws:acm:ap-southeast-1:684706882628:certificate/fb228fdb-3d42-4c8f-a1d8-4b47887ecfd1"),
+          },
+        },
         environment: {
           DATABASE_URL: process.env.DATABASE_URL!,
           NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL!,
