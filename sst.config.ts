bb340e4ed06d0d75cb4467628d6bee2c1bd0fc51
TimelesClock <62864967+TimelesClock@users.noreply.github.com>
2024-02-06 10:45:46 +0800

    Update sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -13,13 +13,13 @@ export default {
     app.setDefaultRemovalPolicy("destroy");
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "whiteboard", {
-        customDomain: {
-          domainName: "whiteboard.timeles.codes",
-          isExternalDomain: true,
-          cdk: {
-            certificate: Certificate.fromCertificateArn(stack, "5fadaabb-93fa-4a74-b77e-2ce9ae83692e", "arn:aws:acm:us-east-1:684706882628:certificate/5fadaabb-93fa-4a74-b77e-2ce9ae83692e"),
-          },
-        },
+        // customDomain: {
+        //   domainName: "whiteboard.timeles.codes",
+        //   isExternalDomain: true,
+        //   cdk: {
+        //     certificate: Certificate.fromCertificateArn(stack, "5fadaabb-93fa-4a74-b77e-2ce9ae83692e", "arn:aws:acm:us-east-1:684706882628:certificate/5fadaabb-93fa-4a74-b77e-2ce9ae83692e"),
+        //   },
+        // },
         environment: {
           DATABASE_URL: process.env.DATABASE_URL!,
           NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL!,
